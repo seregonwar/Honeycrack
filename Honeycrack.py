c8f0edc860fb8094f1ac16212a7f9fa3a2cb1dc6
Seregon <109359355+seregonwar@users.noreply.github.com>
2023-05-26 11:45:20 +0200

    Add files via upload

--- Honeycrack.py
+++ Honeycrack.py
@@ -2,106 +2,97 @@ import random
 import time
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
+import psutil
+
+
+app = QApplication([])
 
 def generate_data(process):
-  """Generates a fake influx of continuous internet data using the specified process.
+    proc = psutil.Process(process.pid)
+    conns = proc.connections()
+    return [{
+        'timestamp': time.time(),
+        'source_ip': conn.laddr[0],
+        'destination_ip': conn.raddr[0],
+        'port': conn.laddr[1],
+        'protocol': conn.type,
+        'bytes_sent': random.randint(1, 1000000),
+        'bytes_received': random.randint(1, 1000000)
+    } for conn in conns]
 
-  Args:
-    process: The process to use to generate the data. Valid values are 'random' and 'sequential'.
 
-  Returns:
-    A list of dictionaries, where each dictionary represents a piece of data.
-  """
-  if process == 'random':
-    return generate_random_data()
-  elif process == 'sequential':
-    return generate_sequential_data()
-  else:
-    raise ValueError(f'Invalid process: {process}')
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-def generate_random_data():
-  """Generates a fake influx of continuous internet data using a random process.
+        self.init_ui()
 
-  Returns:
-    A list of dictionaries, where each dictionary represents a piece of data.
-  """
-  return [{
-      'timestamp':
-      time.time(),
-      'source_ip':
-      random.randint(1, 255) * 256 * 256 + random.randint(1, 255) * 256 +
-      random.randint(1, 255),
-      'destination_ip':
-      random.randint(1, 255) * 256 * 256 + random.randint(1, 255) * 256 +
-      random.randint(1, 255),
-      'port':
-      random.randint(1, 65535),
-      'protocol':
-      random.choice(['TCP', 'UDP']),
-      'bytes_sent':
-      random.randint(1, 1000000),
-      'bytes_received':
-      random.randint(1, 1000000),
-  } for _ in range(1000)]
+    def init_ui(self):
+        self.resize(600, 400)
+        self.move(250, 150)
 
-def generate_sequential_data():
-  """Generates a fake influx of continuous internet data using a sequential process.
+        layout = QVBoxLayout()
 
-  Returns:
-    A list of dictionaries, where each dictionary represents a piece of data.
-  """
-  return [{
-      'timestamp': time.time(),
-      'source_ip': i,
-      'destination_ip': i + 1,
-      'port': i % 65535,
-      'protocol': 'TCP' if i % 2 == 0 else 'UDP',
-      'bytes_sent': i,
-      'bytes_received': i,
-  } for i in range(1000)]
+        process_label = QLabel(self, text='Process:')
+        self.process_combobox = QComboBox(self)
+        for proc in psutil.process_iter():
+            self.process_combobox.addItem(proc.name())
+
+        self.timer_label = QLabel(self, text='Time left: 3 days')
+
+        start_button = QPushButton(self, text='Start')
+        stop_button = QPushButton(self, text='Stop')
+
+        start_button.clicked.connect(self.start_timer)
+        stop_button.clicked.connect(self.stop_timer)
+
+        weather_widget = QWidget(self)
+        weather_layout = QHBoxLayout(weather_widget)
+
+        weather_icon = QLabel(weather_widget)
+        weather_icon.setPixmap(QPixmap('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Weather_icon_-_sunny.svg/1200px-Weather_icon_-_sunny.svg.png'))
+
+        weather_text = QLabel(weather_widget)
+        weather_text.setText('Sunny')
+
+        weather_layout.addWidget(weather_icon)
+        weather_layout.addWidget(weather_text)
+
+        layout.addWidget(process_label)
+        layout.addWidget(self.process_combobox)
+        layout.addWidget(self.timer_label)
+        layout.addWidget(start_button)
+        layout.addWidget(stop_button)
+        layout.addWidget(weather_widget)
+
+        self.setLayout(layout)
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.update_timer)
+        self.time_left = 3 * 24 * 60 * 60  # 3 days timer
+
+    def start_timer(self):
+        self.timer.start(1000)  # Update every second
+
+    def stop_timer(self):
+        self.timer.stop()
+
+    def update_timer(self):
+        self.time_left -= 1
+
+        if self.time_left == 0:
+            self.timer.stop()
+
+        days = self.time_left // (24 * 60 * 60)
+        self.timer_label.setText(f'Time left: {days} days')
+
 
 def main():
-  app = QApplication([])
+    window = MainWindow()
+    window.show()
 
-  window = QWidget()
-  window.setWindowTitle('Fake Internet Data Generator')
-
-  process_label = QLabel(window, text='Process:')
-  process_combobox = QComboBox(window)
-  process_combobox.addItems(['random', 'sequential'])
-
-  honey_label = QLabel(window, text='Honey:')
-  honey_input = QLineEdit(window)
-
-  start_button = QPushButton(window, text='Start')
-  start_button.clicked.connect(lambda: generate_data(process_combobox.currentText()))
-
-  stop_button = QPushButton(window, text='Stop')
-  stop_button.clicked.connect(window.close)
-
-  weather_label = QLabel(window, text='Weather:')
-  weather_widget = QWidget(window)
-  weather_layout = QHBoxLayout(weather_widget)
-
-  weather_icon = QLabel(weather_widget)
-  weather_icon.setPixmap(QPixmap('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Weather_icon_-_sunny.svg/1200px-Weather_icon_-_sunny.svg.png'))
-
-  weather_text = QLabel(weather_widget)
-  weather_text.setText('Sunny')
-
-  weather_layout.addWidget(weather_icon)
-  weather_layout.addWidget(weather_text)
+    app.exec()
 
 
-process_label.move(10, 10)
-process_combobox.move(100, 10)
-honey_label.move(10, 50)
-honey_input.move(100, 50)
-start_button.move(100, 100)
-stop_button.move(200, 100)
-weather_widget.move(300, 10)
-
-window.show()
-
-app.exec()
-
+main()
